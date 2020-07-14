0b2335914bce8cfc88f8b16e26dc2ce1846e7a00
Lauren Sanders <66180981+mlsterl1@users.noreply.github.com>
2020-07-13 22:16:28 -0500

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -35,16 +35,6 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   		PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp (
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  dept_no VARCHAR(4) NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  		PRIMARY KEY (emp_no, dept_no)
-);
-
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
@@ -54,4 +44,33 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   		PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,	
+  dept_no VARCHAR(4) NOT NULL, 
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,  
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  		PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
 SELECT * FROM departments;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM employees;
+
+SELECT * FROM salaries;
+
+SELECT * FROM titles;
+
